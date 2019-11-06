d42a269a5850c54a359ec8952c273735e267cc74
Ilgiz Mustafin <ilgimustafin@gmail.com>
2019-11-07 01:10:24 +0300

    Init first test

---
+++ src/test/java/service/CourseServiceTest.java
@@ -0,0 +1,36 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseServiceTest {
+
+    @Test
+    void findCourse() {
+    }
+
+    @Test
+    void saveCourse() {
+    }
+
+    @Test
+    void deleteCourse() {
+    }
+
+    @Test
+    void updateCourse() {
+    }
+
+    @Test
+    void findAllCourses() {
+    }
+
+    @Test
+    void addUser() {
+    }
+
+    @Test
+    void findAutoById() {
+    }
+}
