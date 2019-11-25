9c087593a027eab5e7021495f7968fb600494724
Agurall <shenya_59_59@mail.ru>
2019-11-25 22:15:36 +0300

    Test for UC 1,3

--- src/test/java/BotTest.java
+++ src/test/java/BotTest.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import org.mockito.stubbing.Answer;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.User;
@@ -33,7 +36,7 @@ import static org.mockito.Mockito.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.*;
 
-class BotTest {
+public class BotTest {
 
     final long CHAT_ID = 1123;
     final long USER_ID = 123123123;
@@ -51,6 +54,17 @@ class BotTest {
                 )
         );
     }
+/*
+    static Stream<Arguments> createDataForSubscription() {
+        return Stream.of(
+                Arguments.of(
+                        true,false
+                ),
+                Arguments.of(
+                        true, true
+                )
+        );
+    }*/
 
     /**
      * Test that courses are shown when selecting courses for
@@ -117,6 +131,11 @@ class BotTest {
         }
     }
 
+    /**
+     * Test for [Use case #1]
+     * Tests menu which user would see when he enters app
+     * @param userRegistered
+     */
     @ParameterizedTest
     @ValueSource(booleans = {true, false})
     public void testStart(boolean userRegistered) {
@@ -157,6 +176,142 @@ class BotTest {
             }
         }
 
-        assertThat(buttonTexts, containsInAnyOrder("Choose course", "All courses"));
+        assertThat(buttonTexts, containsInAnyOrder("Choose course", "All courses", "Unsubscribe"));
     }
+
+    /**
+     * Test for [Use case #3]
+     * Tests subscription and unsubscribtion from course
+     * @param isSubscribedForCourse
+     */
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    public void testCourseSelection(boolean isSubscribedForCourse) {
+        Set<Course> courses = new HashSet<Course>();
+        final boolean subscribtionPerformed = false;
+        boolean unsubscriptionPerformed = false;
+        Bot bot = new Bot();
+
+
+        User user = mock(User.class);
+        when(user.getId()).thenReturn((int) USER_ID);
+
+        model.User userModel = mock(model.User.class);
+        when(userModel.getId()).thenReturn(USER_ID);
+        when(userModel.getChat_id()).thenReturn(CHAT_ID);
+        when(userModel.getCourses()).thenReturn(courses);
+
+        Message message = mock(Message.class);
+        Update update = mock(Update.class);
+        CallbackQuery query = mock(CallbackQuery.class);
+
+        when(update.hasMessage()).thenReturn(false);
+        when(update.hasCallbackQuery()).thenReturn(true);
+        when(update.getMessage()).thenReturn(message);
+        //update.getCallbackQuery().getData()
+        when(update.getCallbackQuery()).thenReturn(query);
+        when(query.getData()).thenReturn("1");
+
+        when(message.getChatId()).thenReturn(CHAT_ID);
+        when(message.getFrom()).thenReturn(user);
+
+        UserService userServiceMock = mock(UserService.class);
+        //if (userRegistered) {
+        when(userServiceMock.findUser(USER_ID)).thenReturn(userModel);
+        //} else {
+        //    when(userServiceMock.findUser(USER_ID)).thenReturn(null);
+        //}
+        if (isSubscribedForCourse) {
+            when(userServiceMock.getCourseById(userModel,1)).thenReturn(new Course("Name",1));
+        } else {
+            when(userServiceMock.getCourseById(userModel,1)).thenReturn(null);
+        }
+        ArgumentCaptor<Course> argument = ArgumentCaptor.forClass(Course.class);
+        doAnswer((Answer) invocation -> {
+            if(isSubscribedForCourse)
+            {
+                fail();
+            }
+            else
+            {
+                Course arg1 = invocation.getArgument(1);
+                assertEquals(1, arg1.getId());
+                assertEquals("course name", arg1.getName());
+            }
+            return null;
+        }).when(userServiceMock).subscribeUser(anyObject(),argument.capture());
+
+        ArgumentCaptor<Course> argument2 = ArgumentCaptor.forClass(Course.class);
+        doAnswer((Answer) invocation -> {
+            if(!isSubscribedForCourse)
+            {
+                fail();
+            }
+            else
+            {
+                Course arg1 = invocation.getArgument(1);
+                assertEquals(1, arg1.getId());
+                assertEquals("course name", arg1.getName());
+            }
+            return null;
+        }).when(userServiceMock).unSubscribeUser(anyObject(),argument2.capture());
+
+        Bot.setUserService(userServiceMock);
+
+    }
+
+
+    /**
+     * Test for [Use case #3]
+     * Tests subscription and unsubscribtion from course
+     * @param userRegistered
+     */
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    public void testnsubscriptionFromBot(boolean userRegistered) {
+        Bot bot = new Bot();
+
+
+        User user = mock(User.class);
+        when(user.getId()).thenReturn((int) USER_ID);
+
+        model.User userModel = mock(model.User.class);
+        when(userModel.getId()).thenReturn(USER_ID);
+        when(userModel.getChat_id()).thenReturn(CHAT_ID);
+
+        Message message = mock(Message.class);
+        Update update = mock(Update.class);
+        when(update.hasMessage()).thenReturn(true);
+        when(update.getMessage()).thenReturn(message);
+        when(message.hasText()).thenReturn(true);
+        when(message.getText()).thenReturn("Unsubscribe");
+        when(message.getChatId()).thenReturn(CHAT_ID);
+        when(message.getFrom()).thenReturn(user);
+
+        UserService userServiceMock = mock(UserService.class);
+        if (userRegistered) {
+            when(userServiceMock.findUser(USER_ID)).thenReturn(userModel);
+        } else {
+            when(userServiceMock.findUser(USER_ID)).thenReturn(null);
+        }
+
+        Bot.setUserService(userServiceMock);
+
+        SendMessage x = (SendMessage) bot.getMethod(update);
+        ReplyKeyboardMarkup mu = (ReplyKeyboardMarkup) x.getReplyMarkup();
+        List<String> buttonTexts = new ArrayList<>();
+        for (KeyboardRow row : mu.getKeyboard()) {
+            for (KeyboardButton b : row) {
+                buttonTexts.add(b.getText());
+            }
+        }
+
+        assertThat(buttonTexts, containsInAnyOrder("Yes", "No"));
+
+        when(message.getText()).thenReturn("Yes");
+        SendMessage x2 = (SendMessage) bot.getMethod(update);
+        
+
+    }
+
 }
